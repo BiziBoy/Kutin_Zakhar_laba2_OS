0641075b057ff0f52ba69b3373ba4e9719ac8141
BiziBoy <79360682+BiziBoy@users.noreply.github.com>
2022-02-24 14:34:21 +0300

    Написал подбор слова, соответствующего хеш значению

--- Kutin_Zakhar_laba2_OS/Program.cs
+++ Kutin_Zakhar_laba2_OS/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 
 namespace ConsoleApplication1
 {
@@ -8,34 +9,63 @@ namespace ConsoleApplication1
   {
     static void Main(string[] args)
     {
-      while (true)
+      string passwordHash = "1115DD800FEAACEFDF481F1F9070374A2A81E27880F187396DB67958B207CBAD";
+      char[] word = new char[5];
+      for (int i = 97; i < 123; i++)
       {
-        char[] randomWord = new char[5];
-        Random random = new Random();
-        //A-Z (65-90) a-z(97-122) 0-9(48-57)
-        for (int i = 0; i < 5; i++)
+        for (int k = 97; k < 123; k++)
         {
-          randomWord[i] = (char)random.Next(97, 123);
-        }
-        Console.WriteLine(randomWord);
-        using (SHA256 sha256Hash = SHA256.Create())
-        {
-          //Из строки в байтовый массив
-          byte[] sourceBytes = Encoding.ASCII.GetBytes(randomWord);
-          byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
-          string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
-
-          Console.WriteLine("The SHA256 hash of " + randomWord + " is: " + hash);
-          if (hash == "1115DD800FEAACEFDF481F1F9070374A2A81E27880F187396DB67958B207CBAD")
+          for (int l = 97; l < 123; l++)
           {
-            Console.WriteLine("Пароль подобран");
-            break;
+            for (int m = 97; m < 123; m++)
+            {
+              for (int n = 97; n < 123; n++)
+              {
+                for (int p = 0; p < 5; p++)
+                {
+                  switch (p)
+                  {
+                    case 0:
+                      word[p] = 'z';
+                      break;
+                    case 1:
+                      word[p] = 'y';
+                      break;
+                    case 2:
+                      word[p] = 'z';
+                      break;
+                    case 3:
+                      word[p] = (char)m;
+                      break;
+                    case 4:
+                      word[p] = (char)n;
+                      break;
+                    default:
+                      break;
+                  }
+                  Console.Write(word[p]);   
+                }
+                using (SHA256 sha256Hash = SHA256.Create())
+                {
+                  //Из строки в байтовый массив
+                  byte[] sourceBytes = Encoding.ASCII.GetBytes(word);
+                  byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
+                  string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+                  Console.WriteLine("The SHA256 hash of " + hash);
+                  if (hash == passwordHash)
+                  {
+                    Console.WriteLine("пароль подобран");
+                    Thread.Sleep(15000);
+                  }
+                }
+                Console.WriteLine();
+              }
+            }
           }
         }
+        Thread.Sleep(300);
+        
       }
-      
-      
-      Console.ReadLine();
     }
   }
 }
