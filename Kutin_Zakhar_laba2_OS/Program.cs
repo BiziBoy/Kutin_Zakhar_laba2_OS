e8c8a583ebe8361acdc05aa63f0d22b80deb21f3
BiziBoy <79360682+BiziBoy@users.noreply.github.com>
2022-03-01 23:57:39 +0300

    Начал реализовывать паттерн Производитель/Потребитель для многопоточности

--- Kutin_Zakhar_laba2_OS/Program.cs
+++ Kutin_Zakhar_laba2_OS/Program.cs
@@ -2,140 +2,98 @@
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
 
 namespace ConsoleApplication1
-{  
-  class Program
+{
+  class Producer
   {
-    const string PATH = "passwordHashes.txt";
-    static object locker = new();
-    static AutoResetEvent waitHandler = new AutoResetEvent(true);
+    private ChannelWriter<string> Writer;
+    //public string HashString { get; set; }
 
-    static void workWithMultithreading(int _countStream, int _sign) 
+    public Producer(ChannelWriter<string> _writer/*, string _hashString*/)
     {
-      //создаем потоки
-      for (int i = 0; i < _countStream; i++)
-      {
-        Thread myThread = new Thread(passwordGuessing);
-        myThread.Name = $"Поток {i + 1}";   // устанавливаем имя для каждого потока
-        myThread.Start(_sign);
-      }
+      Writer = _writer;
+      //HashString = _hashString;
     }
 
-    /// <summary>
-    /// Считывает хеш-значение из файла, за выбор которого отвечает Sign. Затем подбирает пароль с этим хеш-значением
-    /// </summary>
-    static void passwordGuessing(object _sign)
+    private async Task Run()
     {
-      if (_sign is int intSign) 
+      while (await Writer.WaitToWriteAsync())
       {
-        string[] readText = File.ReadAllLines(PATH);
-        string passwordHash = readText[intSign - 1].ToUpper();
         char[] word = new char[5];
         for (int i = 97; i < 123; i++)
         {
+          word[0] = (char)i;
           for (int k = 97; k < 123; k++)
           {
+            word[1] = (char)k;
             for (int l = 97; l < 123; l++)
             {
+              word[2] = (char)l;
               for (int m = 97; m < 123; m++)
               {
+                word[3] = (char)m;
                 for (int n = 97; n < 123; n++)
                 {
-                  lock (locker)
-                  {
-                    for (int p = 0; p < 5; p++)
-                    {
-                      switch (p)
-                      {
-                        case 0:
-                          word[p] = (char)i;
-                          break;
-                        case 1:
-                          word[p] = (char)k;
-                          break;
-                        case 2:
-                          word[p] = (char)l;
-                          break;
-                        case 3:
-                          word[p] = (char)m;
-                          break;
-                        case 4:
-                          word[p] = (char)n;
-                          break;
-                        default:
-                          break;
-                      }
-                      Console.Write(word[p]);
-                    }
-                    using (SHA256 sha256Hash = SHA256.Create())
-                    {
-                      //Из строки в байтовый массив
-                      byte[] sourceBytes = Encoding.ASCII.GetBytes(word);
-                      byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
-                      string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
-                      //Console.WriteLine("The SHA256 hash of " + hash);
-                      if (hash == passwordHash)
-                      {
-                        Console.WriteLine();
-                        Console.WriteLine("Пароль подобран");
-                        Thread.Sleep(15000);
-                      }
-                    }
-                  }
-                  Console.WriteLine();
+                  word[4] = (char)n;
+                  await Writer.WriteAsync(new string(word));
                 }
               }
             }
           }
         }
+        
       }
-
     }
+  }
 
+
+  class Consumer
+  {
+    private ChannelReader<string> Reader;
+
+    public Consumer(ChannelReader<string> _reader)
+    {
+      Reader = _reader;
+    }
     /// <summary>
-    /// Выводит меню для взаимодействия с пользователем
+    /// Находит хеш str
     /// </summary>
-    static void printMenu() 
+    /// <param name="str"></param>
+    /// <returns></returns>
+    public string foundHash(string str)
     {
-      bool flag = true;
-      while (flag)
+      using (SHA256 sha256Hash = SHA256.Create())
       {
-        Console.WriteLine("1. Выполнить задание.");
-        Console.WriteLine("2. Очистить консоль.");
-        Console.WriteLine("3. Выйти из программы.");
-        Console.Write("Введите пункт меню: ");
-        int menuSign = int.Parse(Console.ReadLine());
-        switch (menuSign)
-        {
-          case 1:
-            Console.WriteLine("Выберите по какому хеш значению SHA-256 подобрать пароль: ");
-            Console.WriteLine("1. 1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad");
-            Console.WriteLine("2. 3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b");
-            Console.WriteLine("3. 74e1bb62f8dabb8125a58852b63bdf6eaef667cb56ac7f7cdba6d7305c50a22f");
-            int sign = int.Parse(Console.ReadLine());
-            Console.Write("Введите количество потоков: ");
-            int countStream = int.Parse(Console.ReadLine());
-            workWithMultithreading(countStream, sign);
-            break;
-          case 2:
-            Console.Clear();
-            break;
-          case 3:
-            flag = false;
-            break;
-          default:
-            Console.WriteLine("Данного пункта нет в меню.");
-            break;
-        }
+        //Из строки в байтовый массив
+        byte[] sourceBytes = Encoding.ASCII.GetBytes(str);
+        byte[] hashBytes = sha256Hash.ComputeHash(sourceBytes);
+        string hash = BitConverter.ToString(hashBytes).Replace("-", String.Empty);
+        //Console.WriteLine("The SHA256 hash of " + hash);
+        return hash;
       }
-
     }
+  }
 
-    static void Main(string[] args)
+  class Program
+  {
+    const string PATH = "passwordHashes.txt";
+
+    static public void Main()
     {
-      printMenu();
+      Console.WriteLine("Выберите по какому хеш значению SHA-256 подобрать пароль: ");
+      Console.WriteLine("1. 1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad");
+      Console.WriteLine("2. 3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b");
+      Console.WriteLine("3. 74e1bb62f8dabb8125a58852b63bdf6eaef667cb56ac7f7cdba6d7305c50a22f");
+      int sign = int.Parse(Console.ReadLine());
+      string[] readText = File.ReadAllLines(PATH);
+      string passwordHash = readText[sign - 1].ToUpper();
+      //Console.WriteLine(c.passwordGuessing(passwordHash));
+      //Console.Write("Введите количество потоков: ");
+      //int countStream = int.Parse(Console.ReadLine());
+    
     }
   }
 }
